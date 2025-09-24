ad5ca80271966f06f7be70597986a77b1669f517
geosoares1994 <geosoares121994@gmail.com>
2025-09-23 22:27:48 -0300

    etapa2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Script carregado com sucesso!");
+const video1 = document.querySelector('#video1');
+const video2 = document.querySelector('#video2');
+const video3 = document.querySelector('#video3');
+const mascara = document.querySelector('.mascara')
+
+
+video1.addEventListener('click', () => {
+    video1.style.width = '600px';
+    video1.style.height = '1000px';
+    mascara.style.visibility = 'visible'
+    video1.style.position = 'static'
+    mascara.style.zIndex = '0'
+    video1.style.zIndex = '100'
+    video1.style.transform = 'translateX(70%)'
+})
+mascara.addEventListener('click', () => {
+    mascara.style.visibility = 'hidden'
+    video1.style.width = '500px'
+    video1.style.height = '600px'
+    video1.style.position = 'static'
+    video1.style.transform = 'translateX(0)'
+})
