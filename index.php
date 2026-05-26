e5bf43ee45fccfe73f373307b1ca144f02f5f5c3
Geovanee <geosoares121994@gmail.com>
2026-05-26 13:36:12 -0300

    atualizado

--- index.php
+++ index.php
@@ -211,39 +211,6 @@ $anoatual = date('Y');
 
     </section> -->
 
-    <footer class="footer">
-      <div class="footer-container">
-        <div class="footer-about">
-          <img src="./assets/img/logo.PNG" alt="Logo Beto Estofados" />
-          <p>
-            Transformando estofados há mais de 20 anos, a <strong>Beto Estofados</strong> é líder em reformas e na
-            construção de sonhos em Maceió. Nosso diferencial está no profissionalismo e na atenção aos detalhes.
-          </p>
-        </div>
-
-        <div id="contato" class="footer-contact">
-          <h3>Contato</h3>
-          <p><strong>📍 Endereço:</strong> Rua Oscalina Maria Silva, 85 Vergel do Lago - Maceió/AL</p>
-          <p><strong>📞 Whatsapp:</strong> (82) 99981-8788</p>
-          <p><strong>✉️ Email:</strong> contato@betoestofados.com</p>
-        </div>
-
-        <div class="footer-social">
-          <h3>Fale conosco</h3>
-          <div class="social-icons">
-            <a
-              href="https://api.whatsapp.com/send?phone=5582999818788&text=Ol%C3%A1%2C%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20com%20voc%C3%AAs."><img
-                src="./assets/img/whatsapp.png" alt="Whatsapp" /></a>
-            <a href="https://www.instagram.com/betoestofadosmcz/"><img src="assets/img/Instagram.png"
-                alt="Instagram" /></a>
-          </div>
-        </div>
-      </div>
-
-      <div class="footer-bottom">
-        <p>&copy; copyright 2025 - <?php echo $anoatual;?> Beto Estofados - Todos os direitos reservados.</p>
-      </div>
-    </footer>
 
   </main>
 
@@ -254,3 +221,7 @@ $anoatual = date('Y');
 </body>
 
 </html>
+<?php
+require 'footer.php';
+
+?>
